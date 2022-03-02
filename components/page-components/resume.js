07379aa8a203d3b9c2440bd4345141bb8b4f93e9
troy <hunter007nz@gmail.com>
2022-03-02 18:05:14 +1300

    finished styling resume section

--- components/page-components/resume.js
+++ components/page-components/resume.js
@@ -1,18 +1,18 @@
 const Resume = (props) => {
 
-    console.log("resumeComp props", props)
+    // console.log("resumeComp props", props)
     const { education, work, skills } = props.resumeData
 
-    console.log("resumeComp education", education)
+    // console.log("resumeComp education", education)
 
     const educationList = education.map((qualification, index) => {
         const { name, completed, institution } = qualification.qualification.data.attributes
-        console.log("resumeComp qualification", qualification.qualification.data.attributes)
+        // console.log("resumeComp qualification", qualification.qualification.data.attributes)
         return (
-            <div className="flex-col  ">
+            <div className=" ">
                 <h3 className="flex justify-center">{institution}</h3>
                 <p className="info flex justify-center ">{name} <span>&bull;</span>
-                    <em className="date flex content-center">{completed}</em>
+                    <em className="date ">{completed}</em>
                 </p>
                 {/* <p>{education.description}</p> */}
             </div>
@@ -24,12 +24,12 @@ const Resume = (props) => {
         const { title, company, body, duration } = job.job.data.attributes
         console.log("resumeComp job", job.job.data.attributes)
         return (
-            <div key={company} className="flex-col">
+            <div key={company} className="">
                 <h3 className="flex justify-center">{company}</h3>
-                <p className="info flex justify-center">
+                <p className="info flex justify-center  ">
                     {title}<span>&bull;</span> <em className="date">{duration}</em>
                 </p>
-                <p className="info flex justify-center p-5">
+                <p className="info p-5 flex justify-center ">
                     {body}
                 </p>
 
@@ -38,15 +38,14 @@ const Resume = (props) => {
     })
 
     const skillList = skills.map((skill, index) => {
-        // const { name } = skill.skill_categories.data.attributes
-        console.log("resumeComp skill", skill.skill_categories.data)
+        // console.log("resumeComp skill", skill.skill_categories.data)
         const skillCategoryList = skill.skill_categories.data.map((skillCategory, index) => {
             const { name } = skillCategory.attributes
-            console.log("resumeComp skillCategory", name)
+            // console.log("resumeComp skillCategory", name)
             return (
-                <>
+                <div>
                     <h3>{name}</h3>
-                </>
+                </div>
             )
         })
         return (
@@ -59,33 +58,25 @@ const Resume = (props) => {
         )
     })
 
-
-
-
-
     return (
         <section id="resume">
-
-            <div className="education  flex-col justify center  md:flex  md:justify-between">
-                <div className="flex justify-center md:w-1/4 ">
-                    <h1 className="" ><span>Education</span></h1>
+            <div className="">
+            <div className="education md:flex md:justify-between ">
+                <div className=" md:w-1/4">
+                    <h1 className="flex justify-center" ><span>Education</span></h1>
                 </div>
-
                 <div className=" md:w-3/5  ">
-                    <div className="flex-col justify-center p-5">
+                    <div className="">
                         {educationList}
                     </div>
-
                 </div>
             </div>
 
 
-            <div className=" work flex-col justify-center md:flex  md:justify-between ">
-
-                <div className="flex justify-center md:w-1/4 ">
-                    <h1><span>Work</span></h1>
+            <div className=" work md:flex  md:justify-between ">
+                <div className=" md:w-1/4 ">
+                    <h1 className="flex justify-center"><span>Work</span></h1>
                 </div>
-
                 <div className="md:w-3/5 ">
                         {workList}
                 </div>
@@ -93,22 +84,20 @@ const Resume = (props) => {
 
 
 
-            <div className=" skill flex-col  md:flex  md:justify-between">
-
+            <div className=" skill  md:flex  md:justify-between">
                 <div className="md:w-1/4">
-                    <h1><span>Skills</span></h1>
+                    <h1 className="flex justify-center"><span>Skills</span></h1>
                 </div>
 
                 <div className="">
-
                     {/* <p>{skillmessage} </p> */}
-
-                    <div className="">
+                    <div className="md:w-3/5">
                         <ul className="">
                             {skillList}
                         </ul>
                     </div>
                 </div>
+            </div>
             </div>
         </section>
     );
