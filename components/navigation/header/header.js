db3b8b6b65475f4010357787bc80d52afe2e364d
troy <hunter007nz@gmail.com>
2022-03-19 03:14:04 +1300

    finished header animation

--- components/navigation/header/header.js
+++ components/navigation/header/header.js
@@ -1,34 +1,22 @@
 
-import { FaChevronCircleDown } from 'react-icons/fa';
+// import { FaChevronCircleDown } from 'react-icons/fa';
 import { useState, useRef, useEffect } from 'react'
-import classnames from 'classnames';
 import {CSSTransition} from 'react-transition-group';
-
-
-
 
 const Header = () => {
 
-
-
-
   const ref = useRef()
-
-
   const [visable, setVisable] = useState(false);
   const [show, setShow] = useState(false);
-  const [fadeIn, fadeOut] = useState(false);
-  const [navBar, setNavBar] = useState(false);
   const [hidden, setHidden] = useState(false);
 
-
+  console.log("show", show)
 
   const toggleMenu = (event) => {
     event.preventDefault();
     setVisable(!visable);
 
   }
-  // console.log("window", window.innerWidth)
 
   const addBackGroundOnNav = () => {
     const scroll = window.scrollY;
@@ -40,88 +28,27 @@ const Header = () => {
       setShow(true);
     }
     if (scroll === 0) {
-      setHidden(false);
       setShow(false);
+      setHidden(false);
+     
     }
-
   }
-
-  // const setElHeight = () => {
-  //   const h = ref.current.offsetHeight;
-  //   setHeaderElHeight(h);
-  // }
-
 
   /*----------------------------------------------------*/
   /*	Fade In/Out Primary Navigation
   ------------------------------------------------------*/
 
   useEffect(() => {
-    // setElHeight();
-    // window.addEventListener('resize', setElHeight);
     window.addEventListener('scroll', addBackGroundOnNav)
-
     return () => {
-      // window.removeEventListener('resize', setElHeight);
       window.removeEventListener('scroll', addBackGroundOnNav
       )
     }
-
-
   }, [hidden])
 
-
-
-  // const controlNavBar = () => {
-  //   if (window.scrollY > 100) {
-  //     setShow(true);
-  //   } else {
-  //     setShow(false);
-  //   }
-  // }
-
-  // useEffect(() => { 
-  //   window.addEventListener('scroll', controlNavBar);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', controlNavBar);
-  //   }
-
-  // }, [])
-
-  // $(window).on('scroll', function() {
-
-  //   var h = $('header').height();
-  //   var y = $(window).scrollTop();
-  //     var nav = $('#nav-wrap');
-
-  //    if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-  //       nav.fadeOut('fast');
-  //    }
-  //     else {
-  //        if (y < h*.20) {
-  //           nav.removeClass('opaque').fadeIn('fast');
-  //        }
-  //        else {
-  //           nav.addClass('opaque').fadeIn('fast');
-  //        }
-  //     }
-
-  // });
-
-  // console.log("show", show)
-
-
   return (
-
-
     <nav id="nav-wrap ">
-
-      {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a> */}
-
       <header id="home" className="" ref={ref} >
-
         {/* <!-- Mobile menu button --> */}
         <div class="md:hidden flex items-center pl-7 pt-4 fixed " onClick={toggleMenu} >
           <button class="outline-none mobile-menu-button">
@@ -149,14 +76,11 @@ const Header = () => {
           <li><a className="smoothscroll" href="#contact">Contact</a></li>
         </ul> : null}
 
-
         {/* desktop-nav */}
 
-
-       <CSSTransition in={!hidden} className="fade" timeout={300} unmountOnExit >
-           <div className={`md:flex md:justify-center md:pt-2 sticky z-50   `}>
-          
-              <ul id="nav" className={` ${show && 'transition ease-in-out delay-[2000] opaque ' } `}>
+       <CSSTransition in={!hidden} className="" timeout={15} unmountOnExit>
+           <div className={`md:flex md:justify-center md:pt-2 sticky ` }>
+               <ul id="nav" className={`${show && 'opaque  z-50 '}`}> 
                 <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                 <li><a className="smoothscroll" href="#about">About</a></li>
                 <li><a className="smoothscroll" href="#resume">Resume</a></li>
@@ -164,12 +88,10 @@ const Header = () => {
                 <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                 <li><a className="smoothscroll" href="#contact">Contact</a></li>
               </ul>
-       
+
            </div>
          </CSSTransition>
        
-
-
         <div className=" banner flex  items-center  ">
           <div className="banner-text pb-20">
             {/* <img alt="troias-portfolio-header-background" src={headerBackground} /> */}
@@ -189,11 +111,7 @@ const Header = () => {
           <a className="smoothscroll" href="#about"><FaChevronCircleDown /></a>
         </p> */}
       </header>
-
-
-
     </nav>
-
 
   );
 }
